10fbed1963d06474d747356ad386034de27d1a29
mhdriskyjuliandi <mhd.risky.juliandi02@gmail.com>
2021-01-15 11:09:51 +0700

    project pertama

---
+++ MENGHITUNGLUAS&KELILINGPERSEGI.py
@@ -0,0 +1,9 @@
+print("MENGHITUNG LUAS & KELILING PERSEGI")
+
+s = float(input("\nMasukan Panjang Sisi: "))
+
+luas = s**2
+keliling = 4 * s
+
+print("\nLuas Persegi \t\t:",luas)
+print("Keliling Persegi\t:",keliling)
